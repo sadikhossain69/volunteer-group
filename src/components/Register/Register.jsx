47b47d7d986dfa68eb6c4e9e03bab208c36e5985
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-28 04:35:11 +0600

    react icons and registration page designed

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,17 @@
 import './Register.css'
 import React from 'react';
+import { BsGoogle } from 'react-icons/bs'
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is Register!!!</h2>
+        <div style={{ height: '80vh' }} className='d-flex justify-content-center align-items-center' >
+            <div>
+                <h2>This is Register!!!</h2>
+                <button className='w-100 register-button'>
+                    <BsGoogle/>
+                    Contiue with Google
+                </button>
+            </div>
         </div>
     );
 };
