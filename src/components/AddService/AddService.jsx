012943612ddeaa579a181413cfd2ec481201b2b4
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-28 09:53:54 +0600

    successfully adding services

--- src/components/AddService/AddService.jsx
+++ src/components/AddService/AddService.jsx
@@ -1,23 +1,40 @@
 import './AddService.css'
 import React from 'react';
 import PageTitle from '../Shared/PageTitle/PageTitle';
+import axios from 'axios';
 
 const AddService = () => {
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const name = event.target.name.value 
+        const photoUrl = event.target.photoUrl.value
+        console.log(name, photoUrl);
+        const url = `http://localhost:5000/services`
+        axios.post(url, {
+            name: name,
+            photoUrl: photoUrl
+        })
+        .then(response => {
+            console.log({success: 'true',}, response);
+        })
+    }
+
     return (
         <div>
             <PageTitle route='Add Service' />
             <h2 className='text-center my-5' >This is Add service</h2>
             <div className="w-50 mx-auto">
-                <form>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Service Name</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" autoComplete='off' />
+                <form onSubmit={handleSubmit} >
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Service Name</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" autoComplete='off' name='name' />
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Photo URL</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" autoComplete='off' />
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Photo URL</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" autoComplete='off' name='photoUrl' />
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Add Service</button>
                 </form>
             </div>
         </div>
